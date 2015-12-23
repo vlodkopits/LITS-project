ea3d2d0fbb71df3670193a93ae50c57c116f9755
vlodkopits <vlodkopits@gmail.com>
2015-12-24 00:01:38 +0200

    serv

--- mobile/www/js/services.js
+++ mobile/www/js/services.js
@@ -3,7 +3,10 @@ angular.module('starter.services', [])
 
 .factory('Chats', function($http) {
  
-  var chats = [];
+  var chats = var chats = $http.get('http://nm.lviv.ua/pro/adm/mobeventslist.php').success(function(data){ 
+        return data;
+        console.log(data);
+      });
  
   return {
     all: function(){
